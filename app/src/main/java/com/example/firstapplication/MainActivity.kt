aca57780155f77ec9c71d6f1eee163d834a6b0ef
Luis Kaio <ytkkaio16@gmail.com>
2024-09-26 17:50:30 -0300

    columns, rows, modifiers

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,33 +1,11 @@
 package com.example.firstapplication
 
-import android.graphics.drawable.shapes.Shape
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.firstapplication.ui.theme.FirstApplicationTheme
 
@@ -37,42 +15,92 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FirstApplicationTheme {
-                Box(modifier = Modifier.fillMaxSize()){
-                    Text(text = "Oi!!", modifier = Modifier.align(Alignment.TopCenter))
-                    Text(text = "Bem vindo :)", modifier = Modifier.align(Alignment.Center))
-                    Text(text = "Xau", modifier = Modifier.align(Alignment.BottomCenter))
-                }
+
+                /* // Box com modifiers de posicionamento
+                 Box(modifier = Modifier.fillMaxSize()){
+                     Text(text = "Oi!!", modifier = Modifier.align(Alignment.TopCenter))
+                     Text(text = "Bem vindo :)", modifier = Modifier.align(Alignment.Center))
+                     Text(text = "Xau", modifier = Modifier.align(Alignment.BottomCenter))
+                 }*/
+
+               /* // Box com Columns e Row junto com estilização
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp)) {
+
+                    // Esta Column ocupa toda a tela
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+
+                            .background(color = Color.Blue),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Oi",
+                            fontSize = 60.sp,
+                            fontStyle = FontStyle.Italic,
+                            fontWeight = FontWeight.Black
+                        )
+                        Text(
+                            text = "Xau",
+                            modifier = Modifier
+                                .padding(top = 20.dp)
+                                .background(color = Color.Red)
+                        )
+                    }
+
+                    // Row sobreposta no final da tela e no centro horizontal
+                    Row(
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter) // Alinha ao centro horizontal e na parte inferior da tela
+                            .background(color = Color.Gray)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Oi",
+                            fontSize = 60.sp,
+                            fontStyle = FontStyle.Italic,
+                            fontWeight = FontWeight.Black
+                        )
+                        Text(
+                            text = "Xau",
+                            modifier = Modifier
+                                .padding(start = 50.dp) // Espaçamento entre os textos
+                                .background(color = Color.Red)
+                        )
+                    }
+                }*/
+
+
             }
 
+            /*   BoasVindas("Kaio")
+               Button(onClick = { println("A") }, content = {
+                   Text(text = "butao")
+               })
 
+                Icon(painter =  painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null ,tint = Color.Red)
+               Image(painter = painterResource(id =  R.drawable.ic_launcher_background), contentDescription = null)
 
+               TextField(value = "", onValueChange = {}, label = {
+                    Text(text = "Nome")
+               })
 
-
-         /*   BoasVindas("Kaio")
-            Button(onClick = { println("A") }, content = {
-                Text(text = "butao")
-            })
-
-             Icon(painter =  painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = null ,tint = Color.Red)
-            Image(painter = painterResource(id =  R.drawable.ic_launcher_background), contentDescription = null)
-
-            TextField(value = "", onValueChange = {}, label = {
-                 Text(text = "Nome")
-            })
-
-            FloatingActionButton(
-                onClick = {},
-                modifier = Modifier
-                    .border(2.dp, Color.Blue, CircleShape)
-                    .clip(CircleShape)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                    contentDescription = ""
-                )
-            }*/
-
-
+               FloatingActionButton(
+                   onClick = {},
+                   modifier = Modifier
+                       .border(2.dp, Color.Blue, CircleShape)
+                       .clip(CircleShape)
+               ) {
+                   Icon(
+                       painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                       contentDescription = ""
+                   )
+               }*/
 
         }
     }
