c4a1755e00a5458643c78c680face02dcc24a5f3
Luis Kaio <ytkkaio16@gmail.com>
2024-09-30 12:27:32 -0300

    modifiers testes

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -3,14 +3,24 @@ package com.example.firstapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.*
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,6 +28,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.firstapplication.ui.theme.FirstApplicationTheme
+
 //import java.lang.reflect.Modifier
 
 
@@ -90,6 +101,28 @@ class MainActivity : ComponentActivity() {
                          )
                      }
                  }*/
+
+                /*  // Testes Modifiers
+                  Row(modifier = Modifier.fillMaxWidth()) {
+                      Box(
+                          modifier = Modifier
+                              .background(color = Color.Red)
+                              .height(100.dp)
+                              .weight(1f)
+                      ) {}
+
+                      Spacer(modifier = Modifier.width(5.dp))
+
+                      Box(
+                          modifier = Modifier
+                              .background(color = Color.Blue)
+                              .height(100.dp)
+                              .weight(1f)
+                      ) {}
+                  }
+  */
+
+
 
 
             }
